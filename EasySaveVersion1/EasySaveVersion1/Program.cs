b4197523b56dd5714f892e5bd5de96e076a7a59a
Funmilola <funmilolapriscillamariegrace.egue@viacesi.fr>
2022-11-28 15:29:27 +0100

    Version sous Core 3.0

---
+++ EasySaveVersion1/EasySaveVersion1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EasySaveVersion1.Controller;
+
+namespace EasySaveVersion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller.Controller Controller = new Controller.Controller();
+
+            return;
+        }
+    }
+}
