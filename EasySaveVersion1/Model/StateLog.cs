ebace85369690a93ce0de961e5bd00f719e0b60e
Clea <f.clea33@gmail.com>
2022-11-29 09:53:50 +0100

    State log attributes, gets and sets done

--- EasySaveVersion1/Model/StateLog.cs
+++ EasySaveVersion1/Model/StateLog.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace EasySaveVersion1.Model
 {
-    class StateLog
+    class StateLog : EditJSon
     {
         private StateLog instance;
+        private string NameSave;
+        private string TimeDate;
+        private string State;
+        private int NumberFiles;
+        private double Size;
+        private string SourceFile;
+        private string TargetFile;
+
+        //Singleton set up
         private StateLog()
         {
             
@@ -20,5 +29,72 @@ namespace EasySaveVersion1.Model
             return instance;
         }
 
+        //Gets
+        public string GetDefaultPath()
+        {
+            return DefaultPath;
+        }
+        public string GetName()
+        {
+            return NameSave;
+        }
+        public string GetTime()
+        {
+            return TimeDate;
+        }
+        public string GetState()
+        {
+            return State;
+        }
+        public int GetNumberFiles()
+        {
+            return NumberFiles;
+        }
+        public double GetSize()
+        {
+            return Size;
+        }
+        public string GetSource()
+        {
+            return SourceFile;
+        }
+        public string GetTarget()
+        {
+            return TargetFile;
+        }
+
+        //Sets
+        public void SetDefaultPath(string input)
+        {
+            DefaultPath = input;
+        }
+        public void SetName(string input)
+        {
+            NameSave = input;
+        }
+        public void SetTime(string input)
+        {
+            TimeDate = input;
+        }
+        public void SetState(string input)
+        {
+            State = input;
+        }
+        public void SetNumberFiles(int input)
+        {
+            NumberFiles = input;
+        }
+        public void SetSize(double input)
+        {
+            Size = input;
+        }
+        public void SetSource(string input)
+        {
+            SourceFile = input;
+        }
+        public void SetTarget(string input)
+        {
+            TargetFile = input;
+        }
     }
 }
