7cd1b393e746746644cc949df671488d07d2bf00
Clea <f.clea33@gmail.com>
2022-11-29 14:34:14 +0100

    WriteJSON done ; We may need to factorize the entire EditJSON

--- EasySaveVersion1/Model/EditJSon.cs
+++ EasySaveVersion1/Model/EditJSon.cs
@@ -61,11 +61,59 @@ namespace EasySaveVersion1.Model
                     Console.WriteLine("Executing finally block.");
                 }
             }
+            else
+            {
+                Console.WriteLine("This is not a word understood by EasySave");
+            }
         }
-        public void WriteJSON()
+        public void WriteJSON(string StateOrDaily, string TextToWrite)
         {
             //Here is where you write in the State log if you created a save or if you execute a file
             //You write in the Daily log if you finished to execute a save.
+            if (StateOrDaily == "Daily" || StateOrDaily == "daily" || StateOrDaily == "Dailylog" || StateOrDaily == "DailyLog" || StateOrDaily == "dailylog")
+            {
+                try
+                {
+                    StreamWriter sw = new StreamWriter("C:\\Dailylog.json", true, Encoding.ASCII);
+
+                    //Write things in the ()
+                    sw.Write(TextToWrite);
+
+                    sw.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+            }
+            else if (StateOrDaily == "State" || StateOrDaily == "state" || StateOrDaily == "Statelog" || StateOrDaily == "StateLog" || StateOrDaily == "statelog")
+            {
+                try
+                {
+                    StreamWriter sw = new StreamWriter("C:\\Statelog.json", true, Encoding.ASCII);
+
+                    //Write things in the ()
+                    sw.Write(TextToWrite);
+
+                    sw.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong");
+            }
         }
     }
 }
