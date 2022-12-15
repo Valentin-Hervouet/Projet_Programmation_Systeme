295da5ff6152a394dbcb5e61a631fbba50179de3
Funmilola <funmilolapriscillamariegrace.egue@viacesi.fr>
2022-12-15 16:49:09 +0100

    é

--- EasySaveV2/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ EasySaveV2/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -126,7 +126,7 @@ namespace EasySaveV2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EasySaveV2;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EasySaveV2;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
